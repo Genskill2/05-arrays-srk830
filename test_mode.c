110e358208aeada193ac3cb009e78c23151308d7
srk830 <84616986+srk830@users.noreply.github.com>
2021-06-17 11:29:59 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <assert.h>
 
+int min(int a[], int n) {
+  int min = a[0];
+  for(int i = 1; i < n; i++) {
+    if(min > a[i]) min = a[i];
+  }
+  return min;
+}
 
-int mode(int [], int);
+int max(int a[], int n) {
+  int max = a[0];
+  for(int i = 1; i < n; i++) {
+    if(max < a[i]) max = a[i];
+  }
+  return max;
+}
+
+int mode(int a[], int n) {
+  int min = min(a, n);
+  int max = max(a, n);
+  int count[max-min+1];
+  for(int i = 0; i < max-min; i++) {
+    count[i] = 0;
+  }
+  for(int i = 0; i < n; i++) {
+    count[a[i]-min]++;
+  }
+  return max(count, max-min+1)+min;
 
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
