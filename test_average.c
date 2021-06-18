066ed6b9a2c27210a89e3c785019fd3794670c2d
srk830 <84616986+srk830@users.noreply.github.com>
2021-06-18 14:23:47 +0530

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -2,13 +2,7 @@
 #include <assert.h>
 
 
-float average(int a[], int n) {
-  float avg = a[0];
-  for(int i = 1; i < n; i++) {
-    avg += a[i];
-  }
-  return avg/n;
-}
+float average(int [], int);
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
